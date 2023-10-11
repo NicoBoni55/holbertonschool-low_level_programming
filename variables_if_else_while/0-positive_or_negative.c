fde2e319e4d638d43725d4df6021cf31d8f274d0
NicoBoni55 <bonifrank55@gmail.com>
2023-10-11 09:39:05 -0700

    rep

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{ int x =
+stand(time(0))
+x = rand() - RAND_MAX / 2
+printf("%d \n", x);
+return (0);
+}
