7ec1f970dd68a438d27c09e0fbc3288c43bbc350
NicoBoni55 <bonifrank55@gmail.com>
2023-10-11 10:57:09 -0700

    rep

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 }
 else
 {
-printf("Last digit of %d is %d and is less that 6 and not 0\n", n, n % 10);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 }
 return (0);
 }
