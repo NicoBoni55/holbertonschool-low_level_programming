411c68b90cbb32c7e84df8df8ee4ac0116c5fcbd
NicoBoni55 <bonifrank55@gmail.com>
2023-10-12 06:59:38 -0700

    var

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -10,11 +10,11 @@ int main(void)
 	char alphabet = 'a';
 
 	while (alphabet <= 'z')
-	if (alphabet != 'e' && alphabet != 'q')
 	{
 	putchar(alphabet);
 	alphabet++;
 	}
+	if (alphabet != 'e' && alphabet != 'q')
 	putchar('\n');
 	return (0);
 }
