a6819852829d380bade8b26dcabf18041fdeba25
NicoBoni55 <bonifrank55@gmail.com>
2023-10-12 07:33:40 -0700

    var

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char alphabet = 'z';
+while (alphabet <= 'a')
+{
+putchar(alphabet);
+alphabet--;
+}
+putchar('\n');
+return (0);
+}
