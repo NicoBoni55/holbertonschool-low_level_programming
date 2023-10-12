4754a64039fdbb14c8f5c3fd5eb0d4d7a68162e1
NicoBoni55 <bonifrank55@gmail.com>
2023-10-12 10:17:44 -0700

    fun

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,30 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-/**
- *main - entry point
- *
- * Return: Always 0
- */
 
-void _putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void print_alphabet(void)
+void ft_print_alphabet(void)
 {
 	char c;
 
-	for (c = 'a'; c <= 'z'; c++)
+	c = 'a';
+	while (c <= 'z')
 	{
-	_putchar(c);
+	int _putchar(char c);
+	c++;
 	}
-	_putchar('\n');
-}
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
 }
