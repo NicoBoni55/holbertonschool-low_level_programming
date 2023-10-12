05f8d94129f401b5a44e7d18dfb61d749cf5e7bd
NicoBoni55 <bonifrank55@gmail.com>
2023-10-12 06:32:39 -0700

    var

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-for (char c = 'a' ; c <= 'z' ; c++)
-{
-putchar(c);
-}
-for (char c = 'A' ; c <= 'Z' ; c++)
-{
-putchar(c);
-putchar('\n');
-return (0);
-}
+	for (char c = 'a' ; c <= 'z' ; c++)
+	{
+	putchar(c);
+	}
+	for (char c = 'A' ; c <= 'Z' ; c++)
+	{
+	putchar(c);
+	putchar('\n');
+	return (0);
+	}
 }
