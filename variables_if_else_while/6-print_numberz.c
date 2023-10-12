5a8b3b1c231289af76fda860915cd923fdfdf2a8
NicoBoni55 <bonifrank55@gmail.com>
2023-10-12 07:29:11 -0700

    var

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+	putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
