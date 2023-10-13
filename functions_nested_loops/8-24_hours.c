6eb9b2afd5cb1ae2e75b0af8fa195377a8e1974d
NicoBoni55 <bonifrank55@gmail.com>
2023-10-13 09:54:25 -0700

    fun

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,16 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *jack_bauer - prints every minute of the day 
+ *jack_bauer - prints every minute of the day
  *
  *
  * Return:0
  */
 void jack_bauer(void)
 {
-for (int hour = 0; hour < 24; hour++)
+int hour;
+int minute;
+for (hour = 0; hour < 24; hour++)
 {
-for (int minute = 0; minute < 60; minute++)
+for (minute = 0; minute < 60; minute++)
 {
 printf("%.2d:%.2d\n", hour, minute);
 }
