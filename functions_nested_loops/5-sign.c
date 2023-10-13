9644b250d47c6797114bc1be5ca95c231e6cd466
NicoBoni55 <bonifrank55@gmail.com>
2023-10-13 07:30:46 -0700

    fun

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -10,7 +10,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-	putchar('+1');
+	putchar('+');
 	return (1);
 	}
 	else if (n == 0)
@@ -20,7 +20,7 @@ int print_sign(int n)
 	}
 	else
 	{
-	putchar('-1');
+	putchar('-');
 	return (-1);
 	}
 }
