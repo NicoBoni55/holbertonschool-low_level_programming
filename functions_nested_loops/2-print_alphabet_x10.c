348220f4bb527521ae7c0078e370a4c1899552d2
NicoBoni55 <bonifrank55@gmail.com>
2023-10-13 06:24:59 -0700

    fun

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,8 +8,8 @@ void print_alphabet_x10(void)
 {
 	char letter;
 
-	for (int i = 0; i < 10; i++)
 	for (letter = 'a'; letter <= 'z'; letter++)
+	for (int i = 0; i < 10; i++)
 	{
 	_putchar(letter);
 	}
