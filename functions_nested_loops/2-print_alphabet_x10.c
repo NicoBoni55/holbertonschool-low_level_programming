1983ad3d6184c689a404976ad8137170ba23ae56
NicoBoni55 <bonifrank55@gmail.com>
2023-10-13 06:22:14 -0700

    fun

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_alphabet x10 - Print the alphabet from a-z x10
+ *
+ * Return:0
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+
+	for (int i = 0; i < 10; i++)
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	_putchar(letter);
+	}
+	_putchar('\n');
+}
