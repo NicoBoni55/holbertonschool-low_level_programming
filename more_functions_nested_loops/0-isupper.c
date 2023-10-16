90d8a9e5daa66d8cd1b59c61ed7bf8da4e08fa00
NicoBoni55 <bonifrank55@gmail.com>
2023-10-16 07:56:57 -0700

    more

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -7,10 +7,20 @@
  *
  * Return: 1 if c is uppercase, 0 otherwise
  */
-int _isupper(int c)
+int _isupper(int c);
+int main(void)
 {
+char c = 'C';
+
+if (_isupper(c)) {
+	printf("%c is an uppercase character.\n", c);
+	else
+	printf("%c is not an uppercase character.\n", c);
+}	
+int _isupper(int c) {	
 	if (c >= 'A' && c <= 'Z')
 	return (1);
 else
 	return (0);
+}
 }
