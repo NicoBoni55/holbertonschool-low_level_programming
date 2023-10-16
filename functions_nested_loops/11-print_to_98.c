58bf91cc1628ca3e6487dfe3faaf9edad1a28331
NicoBoni55 <bonifrank55@gmail.com>
2023-10-16 06:38:55 -0700

    fun

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -6,19 +6,19 @@
  * Return:0
  */
 void print_to_98(int n)
-{
-if (n >= 98)
-{
-while (n > 98)
-printf("%d ", n--);
-printf("%d,\n", n);
-}
+	{
+	if (n >= 98)
+		{
+		while (n > 98)
+			printf("%d, ", n--);
+			printf("%d \n", n);
+		}
 
-else
+		else
 
-{
-while (n < 98)
-printf("%d ", n++);
-printf("%d,\n", n);
-}
-}
+		{	
+		while (n < 98)
+			printf("%d, ", n++);
+			printf("%d \n", n);
+		}
+	}
