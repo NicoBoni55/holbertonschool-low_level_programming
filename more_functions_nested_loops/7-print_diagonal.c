28e416e8d7b2c9990c3a3a02a7e5e83b0b47b399
NicoBoni55 <bonifrank55@gmail.com>
2023-10-17 07:25:36 -0700

    more

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -13,17 +13,19 @@ void print_diagonal(int n)
 	if (n <= 0)
 	{
 	_putchar('\n');
-	}
-	else
-	{
 	for (i = 0; i <= n; i++)
 	{
-	for (j = 1; j < i; j++)
+	for (j = 1; j <= i; j++)
 	{
 	_putchar(' ');
-	}
 	_putchar('\\');
+
+	if (i == n -1)
+	continue;
+
 	_putchar('\n');
 	}
 	}
+	_putchar('\n');
+}
 }
