cd94ada147f84ced27c0c448c830537042465baf
NicoBoni55 <bonifrank55@gmail.com>
2023-10-17 11:15:35 -0700

    more

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -8,18 +8,21 @@
  */
 void print_triangle(int size)
 {
-	int i, j, k;
+	int i, j;
 	{
-	if (size <= 0)
-	_putchar('\n');
-
-	else
+	if (size > 0)
 
 	for (i = 1; i <= size; i++)
-	for (j = 1; j <= size; j++)
+
+	for (j = size - i; j > 0; j--)
 	_putchar(' ');
-	for (k = 1; k <= i; k++)
+
+	for (j = 0; j < i; j++)
 	_putchar('#');
-	}
+
+	if (i == size)
+	continue;
+
 	_putchar('\n');
+	}
 }
