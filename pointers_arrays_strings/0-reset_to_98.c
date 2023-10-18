492809ead8dd24af8c77619f46b44a79272bfea9
NicoBoni55 <bonifrank55@gmail.com>
2023-10-18 07:28:18 -0700

    pointer

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - function that takes a pointer and updates the value to 98
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	n = 402;
+	printf("n=%d,\n", n);
+	reset_to_98(&n);
+	printf("n=%d,\n", n);
+	return (0);
+}
