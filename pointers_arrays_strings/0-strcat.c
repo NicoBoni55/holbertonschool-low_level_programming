58653aeaec6f4941bb4821bfa9b1c70f2d11a2ea
NicoBoni55 <bonifrank55@gmail.com>
2023-10-23 09:40:05 -0700

    pointer

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcat - function that concatenates two strings
+ *@dest :string 1
+ *@src : string 2
+ *
+ * Return: 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	while (*dest != '\0')
+	{
+	dest++;
+	}
+	while (*src != '\0')
+	{
+	*dest = *src;
+	src++;
+	dest++;
+	}
+	*dest = '\0';
+}
