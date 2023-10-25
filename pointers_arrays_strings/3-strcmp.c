cfd2b0f22b2a85976b9d78e6eca016e48c55e072
NicoBoni55 <bonifrank55@gmail.com>
2023-10-25 06:05:37 -0700

    pointer

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcmp - function that compares two strings
+ *@s1 : first string
+ *@s2 : second string
+ *
+ * Return: 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+int x = *s1 > *s2;
+	
+	
+	while (*s1 != '\0' || *s2 != '\0')
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 > *s2) ? 1 : -1;
+	}
+	s1++;
+	s2++;
+	}
+		return (0);
+}
