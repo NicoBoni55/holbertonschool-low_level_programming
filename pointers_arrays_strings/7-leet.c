486a94c3b99b82d4f1bc8bc5ffaa82d4ec128f09
NicoBoni55 <bonifrank55@gmail.com>
2023-10-26 07:38:52 -0700

    pointer

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -14,13 +14,13 @@ char *leet(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-	for (j = '0'; j < 10; j++)
-	{
-		if (s[i] == letter[j])
+		for (j = 0; j < 10; j++)
 		{
-			s[i] = num[j];
+			if (s[i] == letter[j])
+			{
+				s[i] = num[j];
+			}
 		}
-	}
 	}
 	return (s);
 }
