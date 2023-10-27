fa3640e2ee03a61a2e9e58b5bf815a66b204bb0d
NicoBoni55 <bonifrank55@gmail.com>
2023-10-27 05:57:06 -0700

    pointer

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _memcpy - function that copies memory area.
+ * @dest: variable to dest
+ * @src: variable to search
+ * @n: variable unsigned
+ *
+ * Return: Nothing.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char original_dest = dest;
+
+	while (n--)
+	{
+		*dest++ = *src++;
+		*dest++;
+	}
+	return (original_dest);
+}
