5d760700258d79cbcbf3a669a35b1e71f80b3bfa
NicoBoni55 <bonifrank55@gmail.com>
2023-10-30 09:23:15 -0700

    pointer

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strstr -  function that locates a substring
+ *@haystack : substring
+ *@needle : string
+ *
+ * Return: Always 0.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*n != '\0' && *h == *n)
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+		{
+			return (haystack);
+		}
+	}
+	return ('\0');
+}
