622357b6feb5e5837d56fee151b8c224fb1a2928
NicoBoni55 <bonifrank55@gmail.com>
2023-11-03 15:25:23 -0700

    malloc

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - prints a grid of integers
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **array;
+
+	if (width < 1 || height < 1)
+	{
+		return (NULL);
+	}
+	array = (int **)malloc(width * sizeof(int *));
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < width; i++)
+	{
+		array[i] = (int *)malloc(height * sizeof(int));
+		if (array[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(array[j]);
+			}
+			free(array);
+			return (NULL);
+		}
+	}
+	return (array);
+}
