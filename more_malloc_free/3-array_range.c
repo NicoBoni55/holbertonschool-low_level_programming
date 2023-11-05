333e25ac23419aea953cda4c737a3cfe70ac6637
NicoBoni55 <bonifrank55@gmail.com>
2023-11-05 14:41:36 -0800

    more_malloc

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -15,14 +15,15 @@ int *array_range(int min, int max)
 	int size;
 	int i;
 
-	size = max - min + 1;
-
-	ptr = (int *) malloc(size * sizeof(int));
-
 	if (min > max)
 	{
 		return (NULL);
 	}
+
+	size = max - min + 1;
+
+	ptr = (int *) malloc(size * sizeof(int));
+
 	if (ptr == NULL)
 	{
 		return (NULL);
