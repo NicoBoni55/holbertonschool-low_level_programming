25b5a109f89538c4d8ee56a83dfca9ebfee1e3f8
NicoBoni55 <bonifrank55@gmail.com>
2023-11-07 05:44:05 -0800

    structure

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,4 +1,26 @@
+#ifndef STRUCTS
+#define STRUCTS
+
+/**
+ *struct dog - define a new type of structure
+ *@name : define the name
+ *@age : define the age
+ *@owner : define the owner
+ *
+ *return : 0.
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+#endif
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+#endif
