4f14c39e44f57d55278d16b42340340b36ae0b04
NicoBoni55 <bonifrank55@gmail.com>
2023-11-11 08:08:52 -0800

    function

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - function given as a parameter on each element of an array.
+ * @array: the array
+ * @size : size of function
+ * @action :  pointer to the function
+ *
+ * Return: the sum
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
+/**
+ * print_square - function that print the square
+ * @num: variable whit the number
+ *
+ * return: 0
+ */
+void print_square(int num)
+{
+printf("%d", num * num);
+}
