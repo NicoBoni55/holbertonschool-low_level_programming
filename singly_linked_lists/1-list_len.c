7c4e82217f1943cd1356ab6668935ccc07667b7e
NicoBoni55 <bonifrank55@gmail.com>
2023-11-28 05:15:13 -0800

    linked list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * list_len - function that returns the number of elements
+ * @h : variable of the list
+ *
+ * Return: the number of elements.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+	while (h)
+	{
+		elements++;
+		h = h-> next;
+	}
+	return (elements);
+}
