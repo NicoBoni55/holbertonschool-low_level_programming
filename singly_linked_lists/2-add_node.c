27fcae0b6649b62d76ba5904507334df06214b6a
NicoBoni55 <bonifrank55@gmail.com>
2023-12-01 12:19:38 -0800

    linked list

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -4,33 +4,37 @@
 /**
  * add_node - function that add a new node at the beginning
  * @head : the beginning of the node
- * @str: variable to dulicate 
+ * @str: variable to dulicate
  *
  * Return: NULL if it failed.
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	char *dupl;
-	int len;
+	int len = 0;
 	list_t *start;
 
 	start = malloc(sizeof(list_t));
-
 	if (start == NULL)
 		return (NULL);
 
 	dupl = strdup(str);
 	if (dupl == NULL)
 	{
-	free (start);
-	return (NULL);
+		free(start);
+		return (NULL);
 	}
-	for (len = 0; len[str];)
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len])
+		len++;
 
 	start->str = dupl;
 	start->len = len;
 	start->next = *head;
-	
+
 	*head = start;
 	return (start);
 }
