17b9049da1efc3bc4dff8f8b5814801ad3e8feb1
NicoBoni55 <bonifrank55@gmail.com>
2023-12-03 08:45:16 -0800

    linked list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,5 +1,7 @@
-#include "lists.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "lists.h"
 /**
  * free_list - Frees a list_t list.
  * @head: A pointer to the list_t list.
