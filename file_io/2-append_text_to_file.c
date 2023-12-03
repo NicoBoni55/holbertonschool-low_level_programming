5c06a5212084e9c14b3e16c11ee6786fed034dbf
NicoBoni55 <bonifrank55@gmail.com>
2023-12-03 11:38:07 -0800

    file_io

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		while (text_content[len])
-				len++;
+			len++;
 
 		wrote = write(i, text_content, len);
 		if (wrote == -1)
