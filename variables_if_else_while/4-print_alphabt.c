ca9989997d37e18660c54562cb90a7cfb879bf10
NicoBoni55 <bonifrank55@gmail.com>
2024-02-15 18:41:07 +0000

    variables if

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c = 97;
+
+	for (c = 97; (c <= 122) || (c =! 101 && 113); c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
