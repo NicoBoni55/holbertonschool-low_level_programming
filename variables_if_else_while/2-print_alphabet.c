68851ae1c00347cbba3f7652f87dac495f5ff7d7
NicoBoni55 <bonifrank55@gmail.com>
2024-02-15 17:33:26 +0000

    variables if

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * return: 0
+ *
+ */
+int main(void)
+{
+	char c = 97;
+
+	for (c = 97; c <= 122; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
