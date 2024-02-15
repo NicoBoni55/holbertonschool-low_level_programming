256b92273c13fbfb81c555f10a0fc7e2a671453c
NicoBoni55 <bonifrank55@gmail.com>
2024-02-15 23:44:44 +0000

    variables if

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+}
