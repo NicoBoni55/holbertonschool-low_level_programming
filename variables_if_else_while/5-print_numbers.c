3e2c9fa36f2bb694c3a3199696e409058b8f8e68
NicoBoni55 <bonifrank55@gmail.com>
2024-02-15 23:18:45 +0000

    variables if

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+
+	return (0);
+}
