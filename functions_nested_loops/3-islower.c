8b83d12b24b9bb75f0c88469bd2be3d408cb0672
NicoBoni55 <bonifrank55@gmail.com>
2024-02-16 18:37:15 +0000

    functions_nested_loops

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if(c != ' ')
+	{
+		return(1);
+	}
+	else
+	{
+		return (0);
+	}
+}
