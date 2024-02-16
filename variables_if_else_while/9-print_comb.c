e1afbb4a75f28e98058285c72cff20dc1afd9f36
NicoBoni55 <bonifrank55@gmail.com>
2024-02-16 13:20:08 +0000

    variables if

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,2 +1,25 @@
 #include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b = 44;
+	int c = 32;
 
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+		if (a != 57)
+		{
+			putchar(b);
+			putchar(c);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
