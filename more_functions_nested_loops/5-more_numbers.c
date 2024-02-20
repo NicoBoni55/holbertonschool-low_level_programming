424d3e788724747a764b5349ff9dd42c529c8047
NicoBoni55 <bonifrank55@gmail.com>
2024-02-20 18:23:56 +0000

    more_functions_nested_loops

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers -  prints the numbers, from 0 to 14
+ *
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b >= 10)
+			{
+				_putchar(b / 10 + '0');
+				_putchar(b % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
