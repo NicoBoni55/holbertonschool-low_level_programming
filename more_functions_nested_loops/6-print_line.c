9db48e043d2a26ad5176a92be507549a55b2e5d7
NicoBoni55 <bonifrank55@gmail.com>
2024-02-20 21:34:42 +0000

    more_functions_nested_loops

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line
+ *
+ * Return: 0
+ */
+void print_line(int n)
+{
+	char c;
+
+	for (c = n; c > 0; c--)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}		
