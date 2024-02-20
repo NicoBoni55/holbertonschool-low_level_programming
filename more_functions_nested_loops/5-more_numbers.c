f3d453c887601e64d9bc833105e1b76907e8944c
NicoBoni55 <bonifrank55@gmail.com>
2024-02-20 18:29:54 +0000

    more_functions_nested_loops

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,11 @@ void more_numbers(void)
 
 	for (a = 0; a <= 10; a++)
 	{
-		for (b = '0'; b <= 14; b++)
+		for (b = 0; b <= 14; b++)
 		{
 			if (b >= 10)
-			{
-				_putchar(b / 10 + '0');
-				_putchar(b % 10 + '0');
-			}
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
 		}
 		_putchar('\n');
 	}
