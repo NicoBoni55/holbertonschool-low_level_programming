dfffb2ad71be93611a07330fd5b35a93cb246b43
NicoBoni55 <bonifrank55@gmail.com>
2024-02-20 17:07:44 -0800

    more_functions_nested_loops

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * main - prints the numbers from 1 to 100
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+		printf("%d", a);
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
