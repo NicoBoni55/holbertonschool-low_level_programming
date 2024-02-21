3f59c90ad44c0568219c2f33e1e4436dde78f667
NicoBoni55 <bonifrank55@gmail.com>
2024-02-20 17:30:21 -0800

    more_functions_nested_loops

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,24 +8,28 @@ int main(void)
 {
 	int a;
 
-	for (a = 1; a < 101; a++)
+	for (a = 1; a <= 100; a++)
 	{
 		if (a % 3 == 0 && a % 5 == 0)
 		{
 			printf("FizzBuzz");
 		}
-		else if (a % 5 == 0)
-		{
-			printf("Buzz");
-		}
-		else if (a % 3 == 0)
-		{
-			printf("Fizz");
-		}
-		else
-		{
-		printf("%d", a);
-		}
+			else if (a % 5 == 0)
+			{
+				printf("Buzz");
+			}
+			else if (a % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			else
+			{
+				printf("%d", a);
+			}
+			if (a == 100)
+			{
+				break;
+			}
 		putchar(' ');
 	}
 	putchar('\n');
