98bc12700bce03475890f80e4445520f52491d81
NicoBoni55 <bonifrank55@gmail.com>
2024-02-22 12:49:48 +0000

    pointers_arrays_strings

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,21 +1,15 @@
 #include "main.h"
 /**
  * _puts -  prints a string
- *
+ * @str : string
  * Return: 0
  */
 void _puts(char *str)
 {
-	int i;
-
-	for(i = 0; *str != '\0'; i++)
-	{	
-		_putchar(str[i]);
-
-			if(str[i] <= '\0')
-			{
-				break;
-			}
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
 	}
 	_putchar('\n');
 }
