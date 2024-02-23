11f43ac268a0ab448291ec11bd9a72d92bb279b2
NicoBoni55 <bonifrank55@gmail.com>
2024-02-23 15:05:21 +0000

    pointers_arrays_strings

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str : string
+ *
+ * Return: 0
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int j;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if ((i % 2) == 0)
+	{
+		i++;
+	}
+	j = i / 2;
+	while (str[j] != '\0')
+	{
+		_putchar(str[j]);
+		j++;
+
+	}
+	_putchar('\n');
+}
+
