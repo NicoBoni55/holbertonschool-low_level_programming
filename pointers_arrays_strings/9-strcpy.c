9cab37ebc6d4339f709875938fd12e3cac91158f
NicoBoni55 <bonifrank55@gmail.com>
2024-02-26 15:35:26 +0000

    pointers_arrays_strings

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest : dest
+ * @src : string
+ *
+ * Return: 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
+	int b;
+
+	while (src[a] != '\0')
+		a++;
+
+	for (b = 0; b < a; b++)
+	{
+		dest[b] = src[b];
+	}
+	dest[b + 1] = '\0';
+	return (dest);
+}
