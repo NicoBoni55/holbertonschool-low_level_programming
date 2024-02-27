fa2c0e58ed1b4343cb78fc569b2266f068312608
NicoBoni55 <bonifrank55@gmail.com>
2024-02-27 19:36:42 +0000

    pointers_arrays_strings

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strchr - function that locates a character in a string
+ * @s: string
+ * @c: character
+ *
+ * Return: s or NULL if character not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			if (s[j] == c)
+			{
+				s = s + j;
+				return (s);
+			}
+		}
+	}
+	return ('\0');
+}
