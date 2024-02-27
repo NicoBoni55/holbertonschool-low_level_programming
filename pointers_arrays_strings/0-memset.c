162cd871c089f94a006a7be885af35117f01f5f6
NicoBoni55 <bonifrank55@gmail.com>
2024-02-27 17:30:53 +0000

    pointers_arrays_strings

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - function that fills memory with a constant byte
+ * @b: constant bytes
+ * @s: string
+ * @n: bytes of the memory
+ *
+ * Return: *s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		s[a] = b;
+	}
+	return (s);
+}
