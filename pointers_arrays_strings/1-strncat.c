48e7ffb16fa677d1109136fcfaff50a9229f5524
NicoBoni55 <bonifrank55@gmail.com>
2024-02-27 18:57:30 +0000

    pointers_arrays_strings

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -9,4 +9,17 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
+	int a = 0;
+	int b = 0;
 
+	while (dest[a] != '\0')
+	{
+		b++;
+		a++;
+	}
+	for (a = 0; src[a] != '\0' && a < n; a++)
+	{
+		dest[b++] = src[a];
+	}
+	return (dest);
+}
