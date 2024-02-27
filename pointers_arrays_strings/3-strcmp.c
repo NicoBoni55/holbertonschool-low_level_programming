ec1f0d25a215c4dd9f066c2b7608f0bfd9b63658
NicoBoni55 <bonifrank55@gmail.com>
2024-02-27 12:53:56 +0000

    pointers_arrays_strings

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,38 +1,31 @@
 #include "main.h"
 /**
  * _strcmp - compares two strings
- * s1 : string 1
- * s2 : string 2
+ * @s1 : string 1
+ * @s2 : string 2
  *
  * Return: 0
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
-	int n;
+	int k;
 
-	for (i = 0; *s1 != '\0'; s1++)
+	for (k = 0; s1[k] != '\0' && s2[k] != '\0' && s1[k] == s2[k]; k++)
 	{
-		i++;
 	}
-	for (j = 0; *s2 != '\0'; s2++)
-	{
-		j++;
-	}
-	for (; i < n; n++)
-	{
-	if ((s1[i]) = (s2[j]))
+	return (s1[k] - s2[k]);
+
+	if (s1[k] == '\0' && s2[k] == '\0')
 	{
 		return (0);
 	}
-	else if ((s1[i]) < (s2[j]))
+	else if (s1[k] == '\0')
 	{
-		return (-n);
+		return (-1);
 	}
-	else if ((s1[i]) > (s2[j]))
+	else
 	{
-		return (n);
-	}
+		return (1);
 	}
 	return (0);
 }
