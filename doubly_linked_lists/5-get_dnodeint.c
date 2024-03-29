fdb92f82409b02b04d24c18522be2536dff46de0
NicoBoni55 <bonifrank55@gmail.com>
2024-03-29 21:05:28 +0000

    doubly_linked_lists

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -8,13 +8,14 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	if (head == NULL)
-		return (NULL);
-
 	while (index)
 	{
-		head = head->next;
-		index--;
+
+		if (head == NULL)
+			return (NULL);
+
+	head = head->next;
+	index--;
 	}
 	return (head);
 }
