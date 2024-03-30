5d153b2a0c7bdfe454b11b0fb75da8ae6b6c04da
NicoBoni55 <bonifrank55@gmail.com>
2024-03-30 05:46:29 +0000

    file_io

---
+++ file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: name for file
+ * @text_content: text to put into file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, file, len = 0;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+		{
+			file = write(fd, text_content, len);
+			len++;
+		}
+	}
+
+	if (file == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
