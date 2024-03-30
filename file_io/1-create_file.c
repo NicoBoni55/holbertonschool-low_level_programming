2586a8dcb421c806aabadf912870f8af70fab4f0
NicoBoni55 <bonifrank55@gmail.com>
2024-03-30 14:59:49 +0000

    file_io

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 	int fd, file, len = 0;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 
