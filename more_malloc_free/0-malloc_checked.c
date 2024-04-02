ceda831a7904c7091c235231ccf55c801face209
NicoBoni55 <bonifrank55@gmail.com>
2024-04-02 23:23:04 +0000

    more_malloc_free

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b : int
+ *
+ * Return: pointer or it fails value of 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *str;
+
+	str = malloc(b);
+
+	if (str != NULL)
+		return (str);
+
+	exit (98);
+}
